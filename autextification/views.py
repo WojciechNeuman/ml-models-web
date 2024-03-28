35a25d03e79b4564db8aabef2662f3d5faed1530
Neuman, Wojciech <wojciech.neuman@gmail.com>
2024-03-28 23:54:21 +0100

    Added secure and more efficient redirection between pages and result pages

--- autextification/views.py
+++ autextification/views.py
@@ -7,44 +7,34 @@ import io
 import os
 
 
-vect = CountVectorizer()
-
 model = load('./savedModels/firstModel.joblib')
 vectorizer = load('./savedModels/vectorizer.joblib')
 model_digit = load('./savedModels/sgd_digits.joblib')
 
-print(type(vectorizer))
 
 def predictor(request):
     return render(request, 'predictor.html')
 
 def autextification_page(request):
+    if request.method == 'POST':
+        text = request.POST['text_in_english']
+
+        x_test = pd.Series([text])
+
+        y_pred = vectorizer.transform(x_test)
+
+        y_pred = model.predict(y_pred)
+
+        if y_pred[0] == 0:
+            y_pred = 'Human'
+        else:
+            y_pred = 'Computer'
+
+        return render(request, 'autextification.html', {'result': y_pred})
+    
     return render(request, 'autextification.html')
 
 def digit_recognizer_page(request):
-    return render(request, 'digit_recognizer.html')
-
-def autextification_result(request):
-    text = request.GET['text_in_english']
-    # need stop engine wait stops would Check safe
-
-    x_test = pd.Series([text])
-
-    y_pred = vectorizer.transform(x_test)
-
-    print(y_pred)
-
-    y_pred = model.predict(y_pred)
-
-    if y_pred[0] == 0:
-        y_pred = 'Human'
-    else:
-        y_pred = 'Computer'
-
-    return render(request, 'autextification_result.html', {'result': y_pred})
-
-def digit_recognizer_result(request):
-
     if request.method == 'POST':
         # Check if the form was submitted using POST method
         if 'image' in request.FILES:
@@ -52,25 +42,23 @@ def digit_recognizer_result(request):
             uploaded_image = request.FILES['image']
             
             pil_image = Image.open(uploaded_image)
-
             grayscale_image = pil_image.convert('L')
-
             final_image = grayscale_image.resize((28, 28))
-
 
             df_image = image_to_dataframe(final_image)
 
             y_pred = model_digit.predict(df_image)[0]
 
+            return render(request, 'digit_recognizer.html', {'result': y_pred})
+
         else:
-            # Handle the case where 'image' key is not found in request.FILES
             print("No image file uploaded")
     else:
         # Handle the case where the form was not submitted using POST method
         print("Form not submitted using POST method")
 
 
-    return render(request, 'digit_recognizer_result.html', {'result': y_pred})
+    return render(request, 'digit_recognizer.html')
 
 
 def image_to_dataframe(image):
