937965ae254f75c49e733378b183864f0293fd78
Wojciech Neuman <wojciech.neuman@gmail.com>
2024-04-05 23:57:54 +0200

    Debugging dropdown menu

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -2,22 +2,36 @@ function toggleDropdown() {
     document.getElementById("dropdown-content").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-    console.log("HALO");
-    if (!event.target.matches('.menu-icon')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
+// window.onclick = function(event) {
+//     console.log("HALO");
+//     if (!event.target.matches('.menu-icon')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
  
-let dropdownMenu = document.getElementById("dropdownMenu");
+// let dropdownWrap = document.getElementById("dropdownWrap");
+
+// function toggleMenu() {
+//     console.log("HALO");
+//     console.log("Halo" + dropdownWrap);
+//     dropdownWrap.classList.toggle("open-menu");
+// } 
 
 function toggleMenu() {
-    dropdownMenu.classList.toggle("open-menu")
-} 
+    let dropdownWrap = document.getElementById("dropdownWrap");
+    console.log("HALO");
+    console.log("Halo" + dropdownWrap);
+    dropdownWrap.classList.toggle("open-menu");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Attach click event listener to the menu icon
+    document.querySelector(".menu-icon").addEventListener("click", toggleMenu);
+});
